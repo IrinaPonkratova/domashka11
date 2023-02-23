d93a567430a3954c775c043fb7cfb1b07cbac0fe
IrinaPonkratova <ira.ponkratova97@mail.ru>
2023-02-23 16:30:19 +0300

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+//все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+//M = 1; N = 5 -> "2, 4"
+//M = 4; N = 8 -> "4, 6, 8"
+
+
+
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message + ":");
+    return int.Parse(Console.ReadLine());
+}
+
+void ShowNumbers(int start, int finish)
+{
+    if (start > finish)
+    {
+        return;
+    }
+    for (int i = start; i < finish; i++)
+    {
+        if (i % 2 == 0)
+        {
+            System.Console.WriteLine(i);
+        }
+    }
+    
+    System.Console.Write($"{start};");
+    ShowNumbers(start + 1, finish);
+}
+int number1 = Prompt("Введите число N");
+int number2 = Prompt("Введите число M");
+
+ShowNumbers(number1, number2);
---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int Prompt (string message)
+{
+    System.Console.WriteLine(message + ":");
+    return int.Parse(Console.ReadLine());
+}
+void Sum(int start, int finish)
+{
+    if (start > finish)
+    {
+        return;
+    }
+    int sum = 0;
+    for (int i = start; i < finish; i++)
+    {
+        sum += i;
+    }
+    
+    System.Console.Write($"{sum};");
+    ShowNumbers(start + 1, finish);
+}
+int number1 = Prompt("Введите число N");
+int number2 = Prompt("Введите число M");
+
+Sum(number1, number2);
